33d167d6fd49ea30dff09065e0c22140e0ea48bf
Ahsilei Pinzon <pinzonahsilei09302003@gmail.com>
2025-11-05 22:20:50 +0800

    Update Result Screen, Capture Result Screen and Added Record Screen, Image Selection Screen and Share Screen.

--- kivy-lcd-app/app/screens/capture_result_screen.py
+++ kivy-lcd-app/app/screens/capture_result_screen.py
@@ -1,4 +1,7 @@
 from kivy.uix.screenmanager import Screen
 
 class CaptureResultScreen(Screen):
-    pass
+    def open_result(self):
+        app = App.get_running_app()
+        app.last_screen = 'capture_result'
+        self.manager.current = 'result'
